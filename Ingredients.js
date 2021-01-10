31db33a8bf43cbfba09d954685bfd2ad5627f518
Tarun-175 <75198662+Tarun-175@users.noreply.github.com>
2021-01-10 10:08:26 +0530

    Add files via upload

---
+++ Ingredients.js
@@ -0,0 +1,10 @@
+1 cup basmati rice, soaked for 30 minutes
+300 grams chicken, skinned and cut into pieces (with bone is better)
+1/2 cup ghee (to use as needed)
+1 cup sliced onions
+1/4 cup cashew nuts
+3 tbsp raisins
+3 tbsp warm milk
+A pinch saffron
+1/4 cup chopped mint leaves
+1/4 cup chopped coriander leaves (cilantro)
---
+++ Recipe.js
@@ -0,0 +1,36 @@
+1.Wash the chicken pieces
+2.In a blender or mixer jar, add all the ingredients
+ needed for marination + salt
+3.Grind coarsely and add to the chicken pieces
+4.Mix well and leave to marinate in the refrigerator
+ for about 8 hours or overnight
+5.Wash and soak 1 cup basmati rice in enough
+ water to completely cover it for 30 minutes
+6.In the meantime, chop your onions and powder the
+ spice masala
+7.Soak a pinch of saffron in 3 tbsp warm milk and 
+set aside
+8.Heat 1/4 cup ghee in a heavy-bottomed pan with
+ a tight-fitting lid. This is the same pan we
+  will use to cook the dum biryani
+9.Add the sliced onions and keep flame at medium-low. Stir frequently to ensure even frying.
+10.While the onions are frying, keep water in a 
+large pot for cooking the rice
+11.Bring the water to a rolling boil and add 1 tsp
+ caraway seeds (shahjeera), 2 bay leaves, 1 tsp oil, and some salt
+12.Add the soaked rice to this and cook on medium
+-high heat until the rice is 3/4 cooked. When you take out one grain and bite into it, it shouldn’t be mushy or cooked fully
+13.While the rice is cooking, keep an eye on the
+ onions and drain when they are fully dark brown
+14.In the same ghee, fry the cashew nuts, drain
+ and then fry the raisins and drain
+15.The rice should now be done. Drain the rice in
+ a colander and fluff it with a fork
+16.Now, in the same pan used for frying the onions,
+ top up the remaining 1/4 cup ghee (see notes)
+17.Add the marinated chicken pieces and mix well
+18.Now add the freshly ground whole spices 
+spices or biryani masala. I added about 2 tbsp of it
+19.Fry for 30 seconds
+20.Add 1 tbsp each of chopped coriander leaves
+ and mint leaves on top
